aac9a9b0f6128100213d546190d9ee2495639298
Rose Seraphin <roseseraphin@yahoo.com>
2019-11-10 17:58:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,10 +45,11 @@ end
 
 def add_s(array)
 array.each_with_index.collect {|s_word, index|  
-if index == 0 or > 1 
+if index == 0 or index > 1 
 return s_word + "s" 
 else 
 s_word == s_word
+end 
 }
 end 
 # if s_word == index[0] && s_word > index[1]
